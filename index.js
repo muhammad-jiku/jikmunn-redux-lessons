fbacc686cd31651be1e70055a47f93cd020d81a2
muhammad-jiku <muhammadjiku364@gmail.com>
2023-02-16 14:10:32 +0600

    reducers functionality added

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//  external import
+const { createStore } = require('redux');
+
 //  defining constants
 const INCREMENT = 'INCREMENT';
 const DECREMENT = 'DECREMENT';
@@ -42,8 +45,23 @@ const reducerCounter = (state = intialCounterState, action) => {
   }
 };
 
+//  create store
+const store = createStore(reducerCounter);
+
+//  subscribing store
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+//  dispatching actions
+store.dispatch(incrementCounter());
+store.dispatch(incrementCounter());
+store.dispatch(incrementCounter());
+store.dispatch(incrementCounter());
+store.dispatch(decrementCounter());
+
 //  redux in a nutshell
 //  1. states
 //  2. dispatch actions
 //  3. reducer (is pure function, who will handle all the logics based on action types)
-//  4. store
+//  4. store ((holding all the states), getState() => to watch the condition of the states , dispatch() => to dispatch actions , subscribe() => to subscribe the stores with the views )
