0dd7bb7f57de4bded9279e206ada8c64d4f5ae83
muhammad-jiku <muhammadjiku364@gmail.com>
2023-02-17 11:05:19 +0600

    redux logger middleware

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
 //  external import
-const { createStore, combineReducers } = require('redux');
+const { createStore, applyMiddleware } = require('redux');
+const { default: logger } = require('redux-logger');
 
 //  defining products constants
 const ADD_PRODUCT = 'ADD_PRODUCT';
 const GET_PRODUCTS = 'GET_PRODUCTS';
 
-//  defining carts constants
-const ADD_CART = 'ADD_CART';
-const GET_CARTS = 'GET_CARTS';
-
 //  products state
 const intialProductState = {
   products: ['Milk', 'Sugar', 'Coffee'],
   numOfProducts: 3,
-};
-
-//  carts state
-const intialCartState = {
-  carts: ['Milk', 'Sugar', 'Coffee'],
-  numOfCarts: 3,
 };
 
 //  actions [object => (type => must be include, payload => payload uses for transfering or receiving data) ]
@@ -34,21 +25,6 @@ const addProduct = (product) => {
   return {
     type: ADD_PRODUCT,
     payload: product,
-  };
-};
-
-//  get carts
-const getCarts = () => {
-  return {
-    type: GET_CARTS,
-  };
-};
-
-// add product
-const addCart = (cart) => {
-  return {
-    type: ADD_CART,
-    payload: cart,
   };
 };
 
@@ -71,33 +47,8 @@ const productReducer = (state = intialProductState, action) => {
   }
 };
 
-//  cart reducer
-const cartReducer = (state = intialCartState, action) => {
-  switch (action.type) {
-    case GET_CARTS:
-      return {
-        ...state,
-      };
-
-    case ADD_CART:
-      return {
-        carts: [...state.carts, action.payload],
-        numOfCarts: state.numOfCarts + 1,
-      };
-
-    default:
-      return state;
-  }
-};
-
-//  combining multiple reducers
-const rootReducer = combineReducers({
-  productR: productReducer,
-  cartR: cartReducer,
-});
-
 //  create store
-const store = createStore(rootReducer);
+const store = createStore(productReducer, applyMiddleware(logger));
 
 //  subscribing store
 store.subscribe(() => {
@@ -109,10 +60,6 @@ store.dispatch(getProducts());
 store.dispatch(addProduct('pen'));
 store.dispatch(addProduct('pencil'));
 store.dispatch(addProduct('geometry box'));
-store.dispatch(getCarts());
-store.dispatch(addCart('table'));
-store.dispatch(addCart('notebook'));
-store.dispatch(addCart('book'));
 
 //  redux in a nutshell
 //  1. states
